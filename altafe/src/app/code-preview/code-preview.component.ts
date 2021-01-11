498afc5593e01cb3c83bcde6540c81182a5c8744
André Suarez <andref.suarez@gmail.com>
2021-01-11 17:52:36 +0000

    added missing unsubscribe

--- altafe/src/app/code-preview/code-preview.component.ts
+++ altafe/src/app/code-preview/code-preview.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { GeneratorService } from '../generator/services/generator.service';
 
 @Component({
   selector: 'app-code-preview',
   templateUrl: './code-preview.component.html'
 })
-export class CodePreviewComponent implements OnInit {
+export class CodePreviewComponent implements OnInit, OnDestroy {
   validationCode = '';
 
+  validationCodeChangedSubscription: Subscription;
   constructor(private generatorService: GeneratorService) {
-    this.generatorService.validationCodeUpdated$.subscribe(code => this.validationCode = code);
+    this.validationCodeChangedSubscription = this.generatorService.validationCodeUpdated$.subscribe(code => this.validationCode = code);
    }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    if (this.validationCodeChangedSubscription) {
+      this.validationCodeChangedSubscription.unsubscribe();
+    }
+  }
 }
